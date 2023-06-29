e835a06d630e3b1a7df1b67251090702a7ac9fda
Maxwell Hardcastle <maxwell0825@gmail.com>
2023-06-28 23:40:32 -0400

    a6 Mongo su23

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -2,29 +2,34 @@ import * as usersDao from "./users-dao.js";
 
 
 const AuthController = (app) => {
-  const register = (req, res) => {
-   const username = req.body.username;
-   const user = usersDao.findUserByUsername(username);
-   if (user) {
-     res.sendStatus(409);
-     return;
-   }
-   const newUser = usersDao.createUser(req.body);
-   req.session["currentUser"] = newUser;
-   res.json(newUser);
- };
+const register = async (req, res) => {
+  const user = await usersDao.findUserByUsername(req.body.username);
+  if (user) {
+    res.sendStatus(403);
+    return;
+  }
+  const newUser = await usersDao.createUser(req.body);
+  req.session["currentUser"] = newUser;
+  res.json(newUser);
+};
 
- const login = (req, res) => {
-   const username = req.body.username;
-   const password = req.body.password;
-   const user = usersDao.findUserByCredentials(username, password);
-   if (user) {
-     req.session["currentUser"] = user;
-     res.json(user);
-   } else {
-     res.sendStatus(404);
-   }
- };
+
+const login = async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    const user = await usersDao.findUserByCredentials(username, password);
+    if (user) {
+      req.session["currentUser"] = user;
+      res.json(user);
+    } else {
+      res.sendStatus(403);
+    }
+  } else {
+    res.sendStatus(403);
+  }
+};
+
 
  const profile = (req, res) => {
    const currentUser = req.session["currentUser"];
@@ -40,13 +45,17 @@ const AuthController = (app) => {
    res.sendStatus(200);
  };
 
- const update   = (req, res) => { };
+ const update = async (req, res) => { 
+	const user = usersDao.updateUser(req.body._id, req.body);
+	req.session["currentUser"] = user;
+    res.json(user);
+ };
 
 
  app.post("/api/users/register", register);
  app.post("/api/users/login",    login);
  app.post("/api/users/profile",  profile);
  app.post("/api/users/logout",   logout);
- app.put ("/api/users",          update);
+ app.post("/api/users/update",   update);
 };
 export default AuthController;
